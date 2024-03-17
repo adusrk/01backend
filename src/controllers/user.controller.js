063ec917c853e25b968b1a788d21b00541b92bcf
Adnan Haider <Adussrk@gmail.com>
2024-03-18 04:18:18 +0530

    bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are compulsory");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User already exists");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file doesnt exist");
   }
